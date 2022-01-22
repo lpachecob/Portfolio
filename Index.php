b644dfa716644dac169e3c9b22dc5352229b5a4b
Luis Pacheco <lpachecoby@gmail.com>
2022-01-22 16:18:52 -0500

    elementos del sitio

---
+++ Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include_once("Components/head.html");?>
+<body>
+<?php 
+include_once("Components/header.html");
+include_once("Components/about.html");
+include_once("Components/resume.html");
+include_once("Components/services.html");
+include_once("Components/portfolio.html");
+include_once("Components/contact.html");
+include_once("Components/scripts.html");?>
+</body>
+
+
